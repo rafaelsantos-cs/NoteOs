b5f7bfb9cffbeabc6115588c2650b840ee7a7279
Rafael Santos <rafa77602ieb@gmail.com>
2026-05-26 14:30:25 -0300

    Customize archiso profile for NoteOs

--- archiso/noteos-releng/profiledef.sh
+++ archiso/noteos-releng/profiledef.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue DVD"
+iso_name="noteos"
+iso_label="NOTEOS_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="NoteOs Project <https://github.com/rafaelsantos-cs/NoteOs>"
+iso_application="NoteOs Live/Installer ISO"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
-install_dir="arch"
+install_dir="noteos"
 buildmodes=('iso')
 bootmodes=('bios.syslinux'
            'uefi.systemd-boot')
